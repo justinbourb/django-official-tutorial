d3513d43a88eea1a5373cd8aa22ffe0518f24bfc
justin <justinbour@gmail.com>
2017-07-24 21:21:45 -0400

    Django official tutorial, first commit

---
+++ polls/urls.py
@@ -0,0 +1,16 @@
+#polls/urls.py 5/13/17 Justin Bourbonniere
+
+from django.conf.urls import url
+from . import views
+app_name = 'polls'
+urlpatterns = [
+    # ex: /polls/
+    # the 'name' value as called by the {% url %} template tag
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    #ex: /polls/5/
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
+    #ex: /polls/5/results/
+    url(r'^(?P<pk>[0-9]+)/results/$',views.ResultsView.as_view(), name='results'),
+    #ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote, name='vote'),
+    ]
